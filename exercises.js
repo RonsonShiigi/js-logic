3299745d23c459f43d0e066273deef103baf8257
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2018-10-08 00:51:16 -1000

    pau19

--- exercises.js
+++ exercises.js
@@ -515,7 +515,7 @@ else {
 }
   allStars(players);
   console.log('east ' + east);
-  console.log('west' + west);
+  console.log('west ' + west);
 
 
 
@@ -534,6 +534,26 @@ else {
 */ 
 
   var subOftheDay = ["Teriyaki Chicken", "Spicy Italian", "Turkey", "BMT", "Black Forest Ham", "Meatball Marinara", "Veggie"];
+  
+  
+  function subways(special){
+
+    for(var i =0; i<subOftheDay.length; i++){
+
+      if (i%2 === 1) {
+        console.log(i);
+        special.slice(i, 1,"Classic Tuna");
+        special[i]= 'Classic Tuna';
+
+      }
+      
+    }
+    return special;
+
+  }
+  subways(subOftheDay);
+  console.log(subOftheDay);
+  
 
 
 /*
